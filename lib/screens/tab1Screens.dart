ede5256ef2ccab6aabbd5bd9d3c97ff59fe88604
Javier David <javierdgarciav@gmail.com>
2022-11-13 21:37:18 -0600

    tab1Screens: agregado

---
+++ lib/screens/tab1Screens.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Tabs1Screen extends StatelessWidget {
+  const Tabs1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tabs1Screen'),
+      ),
+    );
+  }
+}

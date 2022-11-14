86576b52842237a169d2474417fc77185496affa
Javier David <javierdgarciav@gmail.com>
2022-11-13 23:00:19 -0600

    Cambio de pantallas

--- lib/screens/tabsScreen.dart
+++ lib/screens/tabsScreen.dart
@@ -41,7 +41,11 @@ class _Navegation extends StatelessWidget {
 class _Pages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegationModel>(context);
+
     return PageView(
+      //TODO: hacer controller
+      controller: navegacionModel.pageController,
       // physics: BouncingScrollPhysics(),
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
@@ -58,11 +62,16 @@ class _Pages extends StatelessWidget {
 
 class _NavegationModel with ChangeNotifier {
   int _paginaActual = 0;
+  PageController _pageController = new PageController();
 
   int get paginaActual => this._paginaActual;
 
   set paginaActual(int valor) {
     this._paginaActual = valor;
+    _pageController.animateToPage(valor,
+        duration: Duration(milliseconds: 300), curve: Curves.linear);
     notifyListeners();
   }
+
+  PageController get pageController => this._pageController;
 }

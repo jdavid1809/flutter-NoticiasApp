088ff3721a5722bbc99b6bf431f3f40fc033e059
Javier David <javierdgarciav@gmail.com>
2022-11-13 22:30:51 -0600

    Implementacion provider

--- lib/screens/tabsScreen.dart
+++ lib/screens/tabsScreen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsScreen extends StatelessWidget {
   const TabsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Pages(),
-      bottomNavigationBar: _Navegation(),
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navegation(),
+      ),
     );
   }
 }
@@ -15,15 +19,18 @@ class TabsScreen extends StatelessWidget {
 class _Navegation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegationModel>(context);
+
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual = i,
       items: [
         BottomNavigationBarItem(
           icon: Icon(Icons.person_outline),
           label: "Perfil",
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline),
+          icon: Icon(Icons.public_outlined),
           label: "Encabezados",
         ),
       ],
@@ -46,5 +53,16 @@ class _Pages extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _NavegationModel with ChangeNotifier {
+  int _paginaActual = 0;
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual(int valor) {
+    this._paginaActual = valor;
+    notifyListeners();
   }
 }

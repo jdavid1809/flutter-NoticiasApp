07f5ac7ab10e5dfc2b105d7067c1ecdb9c295fbb
Javier David <javierdgarciav@gmail.com>
2022-11-13 22:01:31 -0600

    Agregacion del boton

--- lib/screens/tabsScreen.dart
+++ lib/screens/tabsScreen.dart
@@ -6,17 +6,45 @@ class TabsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-        ],
-      ),
+      body: _Pages(),
+      bottomNavigationBar: _Navegation(),
+    );
+  }
+}
+
+class _Navegation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: "Perfil",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: "Encabezados",
+        ),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // physics: BouncingScrollPhysics(),
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
     );
   }
 }

2838bb57249be152c360e432513983dbdb924e9c
Javier David <javierdgarciav@gmail.com>
2022-11-16 00:09:24 -0600

    agregamos imagenes para prueba de errores

--- lib/theme/myTheme.dart
+++ lib/theme/myTheme.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 final miTema = ThemeData.dark().copyWith(
   // accentColor: Colors.red,
+
   colorScheme: ColorScheme.dark(),
 );
--- lib/widgets/listaNoticias.dart
+++ lib/widgets/listaNoticias.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noticias_app/theme/myTheme.dart';
 import '../models/models.dart';
 
 class ListaNoticias extends StatelessWidget {
@@ -11,8 +12,96 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: this.noticias.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text(this.noticias[index].title);
+        return _Noticia(noticia: this.noticias[index], index: index);
       },
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        _TarjetaTopBar(noticia, index),
+        _TarjetaTitulo(noticia),
+        _TarjetaImagen(noticia),
+      ],
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaImagen(this.noticia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: FadeInImage(
+        placeholder: AssetImage('assets/giphy.gif'),
+        image: AssetImage('assets/giphy.gif'),
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaTitulo(this.noticia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        noticia.title,
+        style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+      ),
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar(
+    this.noticia,
+    this.index,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      margin: EdgeInsets.only(top: 15),
+      child: Row(
+        children: <Widget>[
+          Text(
+            '${index + 1}. ',
+            style: TextStyle(
+              color: miTema.colorScheme.secondary,
+              // fontSize: 30
+            ),
+          ),
+          Text(
+            '${noticia.source.name}. ',
+            style: TextStyle(
+                // color: miTema.colorScheme.secondary,
+                // fontSize: 30
+                ),
+          ),
+        ],
+      ),
     );
   }
 }

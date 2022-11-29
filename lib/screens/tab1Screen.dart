3b974ab84145dcf887cc19c2001b0bac1891c3c6
Javier David <javierdgarciav@gmail.com>
2022-11-29 01:36:58 -0600

    Mostrar pagina de carga

--- lib/screens/tab1Screen.dart
+++ lib/screens/tab1Screen.dart
@@ -3,14 +3,27 @@ import 'package:noticias_app/services/services.dart';
 import 'package:noticias_app/widgets/listaNoticias.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Screen extends StatelessWidget {
+class Tab1Screen extends StatefulWidget {
+  @override
+  State<Tab1Screen> createState() => _Tab1ScreenState();
+}
+
+class _Tab1ScreenState extends State<Tab1Screen>
+    with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     final headlines = Provider.of<NewsService>(context).headlines;
     // newsServices.headlines;
 
     return Scaffold(
-      body: ListaNoticias(headlines),
+      body: (headlines.length == 0)
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListaNoticias(headlines),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }

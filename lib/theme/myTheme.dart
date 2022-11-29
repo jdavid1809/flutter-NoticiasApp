1b2b82fd4eb1bf289eac4c162f9f093e2dbc6cf5
Javier David <javierdgarciav@gmail.com>
2022-11-29 01:30:56 -0600

    agregamos descripcion y botones

--- lib/theme/myTheme.dart
+++ lib/theme/myTheme.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 
 final miTema = ThemeData.dark().copyWith(
   // accentColor: Colors.red,
-
   colorScheme: ColorScheme.dark(),
 );
--- lib/widgets/listaNoticias.dart
+++ lib/widgets/listaNoticias.dart
@@ -31,7 +31,69 @@ class _Noticia extends StatelessWidget {
         _TarjetaTopBar(noticia, index),
         _TarjetaTitulo(noticia),
         _TarjetaImagen(noticia),
+        _TarjetaBody(noticia),
+        _TarjetaBotones(),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(),
       ],
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RawMaterialButton(
+            onPressed: () {},
+            fillColor: miTema.accentColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(
+              Icons.star_border_outlined,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          RawMaterialButton(
+            onPressed: () {},
+            fillColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(
+              Icons.more,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaBody(this.noticia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Text((noticia.description != null) ? noticia.description! : ""),
+      //MI FORMA
+      // child: (noticia.description != null)
+      //     ? Text(
+      //         noticia.description!,
+      //         style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+      //       )
+      //     : Text(""),
     );
   }
 }
